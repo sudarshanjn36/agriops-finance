a51008efe2407dcff80d0b6955e1d61d29e8197c
Sudarshan <sudarshanjn36@gmail.com>
2026-05-08 13:56:27 -0400

    Final changes, ready for deoployment

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -1,9 +1,10 @@
+import os
 import streamlit as st
 import requests
 import pandas as pd
 import plotly.express as px
 
-API_URL = "http://backend:8000"
+API_URL = os.getenv("API_URL", "http://backend:8000")
 
 st.set_page_config(page_title="AgriOps Finance", layout="wide")
 
